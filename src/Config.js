12bca14a62b0dfa5d82c972f7fe367e1a2c803f0
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2021-03-11 04:25:00 +0000

    Bump backend to fix coinbase scanning bug

--- src/Config.js
+++ src/Config.js
@@ -209,7 +209,7 @@ const Config = new function() {
     /**
      * Displayed in the settings screen
      */
-    this.appVersion = 'v1.2.1';
+    this.appVersion = 'v1.2.2';
 
     /**
      * Base URL for us to chuck a hash on the end, and find a transaction
